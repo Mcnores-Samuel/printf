1c5d5441969ced0c376cce4b9622796f222c82cd
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-03-29 01:47:40 +0200

    implementation of printf function

--- printf.c
+++ printf.c
@@ -16,15 +16,28 @@ int _printf(const char *format,...)
 
 	va_start(args, format);
 
+	if (format == NULL)
+		return (-1);
+
 	while (format && format[i])
 	{
 		if (format[i] == '%')
 		{
 			i++;
+
+			if (!(format[i]))
+				return (-1);
+
 			fun_ptr = get_function(format[i]);
 			if (fun_ptr)
 			{
 				bytes += fun_ptr(args);
+			}
+			else
+			{
+				print('%', 1);
+				print(format[i], 1);
+				bytes += 2;
 			}
 		}
 		else
